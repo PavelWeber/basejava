ea37eed24920801072302a9437b67218eae5208f
Pavel Tkalin <pavel.tkalin@electrolux.com>
2019-08-28 17:27:30 +0300

    JavaIntro

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,64 @@
 /**
  * Array based storage for Resumes
  */
+
+import java.util.Arrays;
+
+
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+
+    private int counter = 0;
+
+    Resume[] storage = new Resume[10];
 
     void clear() {
+        for (int i = 0; i < counter; i++) {
+            storage[i] = null;
+        }
+        counter = 0;
     }
 
     void save(Resume r) {
+        if (r.uuid != null) {
+            storage[counter] = r;
+            counter++;
+        } else System.out.println("Please input not null Resume");
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < counter; i++) {
+            if ((storage[i].uuid).equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
+
     void delete(String uuid) {
+        for (int i = 0; i < counter; i++) {
+            if ((storage[i].uuid).equals(uuid)) {
+                (storage[i].uuid).equals(null);
+                counter--;
+                System.arraycopy(storage, i + 1, storage, i, counter - i);
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, counter);
     }
 
     int size() {
-        return 0;
+        int j = 0;
+        for (Resume r : storage) {
+            if (r != null) {
+                j++;
+            }
+        }
+        return j;
     }
 }
